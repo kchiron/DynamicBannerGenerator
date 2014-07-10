58cac45ee8ac13a8ddd316e5857fa0633eb05daa
kchiron <katia.chiron@gmail.com>
2014-07-10 15:18:07 +0200

    Start development of weather management

--- src/main/java/dbg/data/media/element/generated/WeatherElement.java
+++ src/main/java/dbg/data/media/element/generated/WeatherElement.java
@@ -1,10 +1,18 @@
 package dbg.data.media.element.generated;
 
 import dbg.data.WeatherLocation;
+import dbg.data.property.PropertyManager;
 import dbg.data.property.WeatherProperties;
+import dbg.exception.ImageGenerationException;
+import dbg.image.ImagePanel;
 import dbg.ui.LocalizedText;
+import dbg.ui.util.UiUtils;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 
 import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class WeatherElement extends GeneratedMediaElement {
 
@@ -41,7 +49,35 @@ public class WeatherElement extends GeneratedMediaElement {
 	}
 
 	@Override
-	public File generateImage() {
-		return null;
+	public File generateImage() throws ImageGenerationException, IOException {
+	    System.out.println("TYPE : " + type.toString());
+	    BufferedImage img = null;
+	    try {
+		    File bckgndImg = new File(getClass().getResource("bckgndWeather.jpg").getPath());
+		    File file = PropertyManager.getWeatherProperties().setBackgroundImage(bckgndImg);
+		    img = ImageIO.read(file);
+	    } catch (Exception e) {
+		    throw new ImageGenerationException(e.getMessage());
+	    }
+
+	    ImagePanel panel = new ImagePanel(img);
+	    //Getting the size of the image (=panel size)
+	    final Dimension d = panel.getPreferredSize();
+		
+		    
+	    double scaleX = d.width / 1920.0; //<taille du layout>;
+	    double scaleY = d.width / 1080.0; //<taille du layout>;
+	    double scale = Math.min(scaleX, scaleY);
+	    int shiftX = scale > 1.0 ? (int) ((d.width - 1920.0 * scale) / 2.0) : 0;
+	    int shiftY = scale > 1.0 ? (int) ((d.height - 1080.0 * scale) / 2.0) : 0;
+
+	    //Scale panel and all its components to fit the size of the background
+	    UiUtils.scaleAndShiftComponents(panel, scale, shiftX, shiftY);
+
+	    // Create a temporary file (in tmp folder of the current OS) and export image to this file
+	    File out = File.createTempFile("DBG-horoscopePage-", ".png");
+	    panel.exportToImage(out, "png");
+
+	    return out;
 	}
 }
---
+++ src/test/java/dbg/data/media/element/generated/WeatherElementTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbg.data.media.element.generated;
+
+import dbg.data.WeatherLocation;
+import dbg.data.property.WeatherProperties.Type;
+import dbg.exception.ImageGenerationException;
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Katia
+ */
+public class WeatherElementTest {
+    
+    public WeatherElementTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of generateImage method, of class WeatherElement.
+     */
+    @Test
+    public void testGenerateImage() throws ImageGenerationException, IOException {
+	System.out.println("generateImage");
+	WeatherElement element = new WeatherElement(new WeatherLocation("France", "Auvergne", "Chappes", 45.86, 3.21), 10);
+	element.setType(Type.CITY);	
+	File expResult = null;
+	File result = element.generateImage();
+	assertEquals(expResult, result);
+	// TODO review the generated test code and remove the default call to fail.
+	fail("The test case is a prototype.");
+    }
+}
+
